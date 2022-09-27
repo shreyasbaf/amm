536f6d9ef2b6a55d366311860926e848230d8f5a
shreyasbaf <shreyas7bafna@gmail.com>
2022-09-27 10:05:04 +0530

    fix: make add as default

--- src/pages/Liquidity.tsx
+++ src/pages/Liquidity.tsx
@@ -46,7 +46,7 @@ const Liquidity = () => {
   const addRecentTransaction = useAddRecentTransaction();
   const BUSTAddress = bustFactoryAddress;
   const RESTAddress = wbnbAddress;
-  const [active, setActive] = useState("Swap");
+  const [active, setActive] = useState("Add");
   const [percentage, setPercentage] = useState<any>(50);
   const [rest, setRest] = useState("");
   const [bust, setBust] = useState("");
